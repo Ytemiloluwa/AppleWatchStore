33bb42df61e6883c5fd88fdb7ac44fd77c0772be
amtello <ytemilouwa@yahoo.com>
2020-05-21 11:01:10 +0100

    Initial Commit

---
+++ AppleWatchStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleWatchStore
+//
+//  Created by Tello on 21/05/2020.
+//  Copyright © 2020 Tello. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
